050f9137c28e67d3dec919efdee3bd8ad78b6e50
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-20 18:33:10 +0530

    Add files via upload

---
+++ GnomeSort.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+public class GnomeSort {
+	
+	 void gnomeSort(int[] nums)
+	  {
+	  int i=1;
+	  int j=2;
+	 
+	  while(i < nums.length) {
+	    if ( nums[i-1] <= nums[i] ) 
+		{
+	      i = j; j++;
+	    } else {
+	      int tmp = nums[i-1];
+	      nums[i-1] = nums[i];
+	      nums[i--] = tmp;
+	      i = (i==0) ? j++ : i;
+	    }
+	  }
+	}
+	  
+	    // Method to test above
+	    public static void main(String args[])
+	    {
+	        GnomeSort ob = new GnomeSort();
+	        int nums[] = {7, -5, 3, 2, 1, 0, 45};
+	        System.out.println("Original Array:");
+	        System.out.println(Arrays.toString(nums));
+	        ob.gnomeSort(nums);
+	        System.out.println("Sorted Array");
+	        System.out.println(Arrays.toString(nums));
+	    }
+	}
+
+
+
