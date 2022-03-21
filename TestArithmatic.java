b4a3358328b1b3e7c2134dc65e249120b0794526
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:25:37 +0530

    Arithmatic.java part-2

---
+++ TestArithmatic.java
@@ -0,0 +1,13 @@
+package arithmatic;
+
+public class TestArithmatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Arithmatic arithmatic=new Arithmatic();
+		int add=arithmatic. add (80,20);
+		System.out.println(add);
+
+	}
+
+}
