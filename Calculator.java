c3079dc06bbf899dcd9d756ee7855c6d152f711a
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:35:25 +0530

    Calculator.java part -1

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+
+public class Calculator {
+	public int add(int i,int j,int k) {
+		int add=i+j+k;
+		return add;
+	}
+	public int sub(int i,int j,int k) {
+		int sub=i-j-k;
+		return sub;
+	}
+	public int mul(int i,int j,int k) {
+		int mul=i*j*k;
+		return mul;
+	}
+
+	public int div(int i,int j,int k) {
+		int div=i/j/k;
+		return div;
+	}
+	public int square(int i,int j) {
+		int square=(i)^2;
+		return square;
+	}
+	public int remindor(int i,int j) {
+		int remindor=i/j;
+		return remindor;
+	}
+	public int 	quotient(int i,int j) {
+		int quotient=i/j;
+		return quotient;
+	}
+}
+		
