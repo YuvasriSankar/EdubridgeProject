7d7c8caf8fdc47335cdce14dea26072364eb917c
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 08:17:16 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
 
 public class Student {
-	String name="yuvasri";
-	int age;
-	int roll_no;
-	char shape;
-	int account;
-	String department;
-	
 
-}
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter StudentMark:");
+			int StudentMark=scanner.nextInt();
+			if(StudentMark>=90 && StudentMark<=100) {
+				System.out.println("Excellent");
+			}else if(StudentMark>=80 && StudentMark<=90 ) {
+				System.out.println("Good");
+}else if(StudentMark>=60 && StudentMark<=80 ) {
+			System.out.println("passed");
+			
+}else if(StudentMark<60 ) {
+			System.out.println("Failed");
+}else
+				System.out.println("No Grade");
+		}
+			
+	
+			
+			
+			
+		}
+			
+
+	}
+
+
