e3a2faaecf26beb82d139f3cb1c4d78ee14ad3ab
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:27:38 +0530

    ArithmaticOperation part-1

---
+++ StaticExample.java
@@ -0,0 +1,39 @@
+package arithmatic1;
+
+public class StaticExample {
+	 int num1=5;
+	 int num2=5;
+	/* public int add(int num1,int num2) {
+			int add=num1+num2;
+			return add;
+		}
+
+	 public int sub(int num1,int num2) {
+			int sub=num1-num2;
+			return sub;
+		}
+	 
+	 public int mul(int num1,int num2) {
+			int mul=num1*num2;
+			return mul;
+		}
+	 public int div(int num1,int num2) {
+			int div=num1/num2;
+			return div;
+		}
+	 public int mod(int num1,int num2) {
+			int mod=num1%num2;
+			return mod;
+		}*/
+	 
+	 
+	 
+	
+	
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}*/
+
+}
