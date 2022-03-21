8d1ab77928a35d5f82b929dd29a4f739f69d9168
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:50:28 +0530

    Student project part-1

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to java program");
+		//classname referencevariable=new classname();
+		Student student;
+		student=new Student();
+		System.out.println(student.name);
+	//classname referencevariable=new classname();
+		Student student1;
+		student1=new Student();
+		student1.name="gomathi";
+		student1.age=20;
+		student1.department="ECE";
+		student1.roll_no=6;
+		System.out.println(student1.name+" "+student1.age+" "+student1.department+" "+student1.roll_no);
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
