1be5843f9939035016005365370d791052862ab4
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:38:35 +0530

    Customer project part-1

--- Account.java
+++ Account.java
@@ -1,24 +1,10 @@
 
 public class Account {
-	String account_name;
-    String account_no;
-	String account_detail;
-	String account_phoneno;
-	double amount_deposit;
-	public void amount_deposit(int i) {
-		amount_deposit=amount_deposit+i;
-		
-	}
-	
-	public  void amount_withdraw(int i) {
-		 amount_deposit=amount_deposit-i;
-		
-	}
-	public double  account_checkbalance() {
-		return amount_deposit;
-		
-	}
-	
-
+	String name="Keerthikshah";
+	String department;
+	Short accountno;
+	String shape;
+	short cost;
+	String variety;
 
 }
