e8eb64490eafae59a993cb3fb77582a04e6e6a42
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:41:23 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fact=1;
+	//factorial formula n! = n*(n-1)
+       for(int i=1;i<=10;i++)
+       {
+    	   fact=fact*i;
+       System.out.println(fact);
+
+	}
+
+}
+}
