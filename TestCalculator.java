dc42dc68a851d955aee8e352e01d99820c605c43
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:36:14 +0530

    Calculator.java part-2

---
+++ TestCalculator.java
@@ -0,0 +1,40 @@
+
+public class TestCalculator {
+  
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Calculator calculator1=new Calculator();
+	int add=calculator1. add (80,40,20);
+	System.out.println(add);
+		
+		Calculator calculator2=new Calculator();
+		int sub =calculator2. sub (80,40,20);
+		System.out.println(sub); 
+		 
+		Calculator calculator3=new Calculator();
+		int mul=calculator3. mul (2,3,4);
+		System.out.println(mul );
+		
+		Calculator calculator4=new Calculator();
+		int div=calculator4. div(100,2,2);
+		System.out.println(div );
+		
+		
+		Calculator calculator5=new Calculator();
+		int square=calculator5. square (4,4);
+		System.out.println(square);
+		
+		Calculator calculator6=new Calculator();
+		int remindor=calculator6. remindor (100,3);
+		System.out.println(remindor);
+		
+		
+		Calculator calculator7=new Calculator();
+		int quotient=calculator7. quotient(100,3);
+		System.out.println(quotient);
+		
+		
+
+	}
+
+}
