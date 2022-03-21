21c5065ba479ff873b27fd1760049437c354dd38
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 08:00:02 +0530

    Number raised power

---
+++ Number.java
@@ -0,0 +1,17 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result=1;
+		int num=2;
+		int raise=3;
+		for(int i=1;i<=raise;i++) {    //2^3=2*2*2=8
+			result=result*num;
+		}
+			System.out.println(result);
+		
+
+	}
+
+}
