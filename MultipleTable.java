1b77f6abf6afcbee0cfb9ba0fbb9277c141fce2b
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:58:45 +0530

    Add files via upload

---
+++ MultipleTable.java
@@ -0,0 +1,20 @@
+
+public class MultipleTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<=100;i++)
+		{
+			if(i%12==0)
+			{
+				System.out.println(i );
+			}
+
+
+				
+		}
+			}
+
+	}
+
+
