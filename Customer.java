0a11e7bf12db0993438f32762b16fbea7ec12e7e
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 07:39:11 +0530

    Customerproject part-2

---
+++ Customer.java
@@ -0,0 +1,68 @@
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub//classname referencevariable=new classname();
+		System.out.println("Welcome to Super Market");
+		
+        Account account=new Account();
+        account.name="Keerthishah";
+        account.department="Biscuits";
+        
+        
+        
+        account.accountno=12345;
+        System.out.println(account.name+" "+account.department+" "+account.shape+" "+account.variety+" "+account.cost+" "+account.accountno);
+        
+        Account account1=new Account();
+        account1.name="Gomathi";
+        account1.department="sweet";
+        account1.shape="Diamond";
+        account1.variety="kaju katli";
+        account1.cost=1000;
+        account1.accountno=6789;
+        System.out.println(account1.name+" "+account1.department+" "+account1.shape+" "+account1.variety+" "+account1.cost+" "+account1.accountno);
+        
+        Account account2=new Account();
+        account2.name="Keerthana";
+        account2.department="chocolate";
+        account2.shape="Rectangle";
+        account2.variety="Dairy Milk";
+        account2.cost=100;
+        account2.accountno=13579;
+        System.out.println(account2.name+" "+account2.department+" "+account2.shape+" "+account2.variety+" "+account2.cost+" "+account2.accountno);
+        
+        
+        
+        
+        		
+        
+        
+ 
+        
+        
+        
+        		
+        
+        
+
+		
+        
+        
+        
+        
+        		
+        
+        
+
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
