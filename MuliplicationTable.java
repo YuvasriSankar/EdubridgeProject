1150e53ac83748178664cd4cbed38bcce1307ff6
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-21 08:07:14 +0530

    Add files via upload

---
+++ MuliplicationTable.java
@@ -0,0 +1,14 @@
+
+public class MuliplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" ");
+		for (int num=1;num<=10;num++)
+		{
+			System.out.print(num*10+ " ");
+		}
+
+	}
+
+}
---
+++ NaturalNumber.java
@@ -0,0 +1,15 @@
+
+public class NaturalNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, sum = 0;
+		for(i=1;i<=10;i++)//natural number formula: n*(n+1)/2w5
+		{
+			sum=sum+i;
+			System.out.println("The Sum of first 10 natural number "+sum);
+		}
+
+	}
+
+}
