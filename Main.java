30091994bca98532ad14fc41439ac818ab648e2a
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-03-24 22:25:18 +0530

    Flush Characters

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
-package fetching_middle_character_from_string;
+package flushcharacters;
 
 import java.util.Scanner;
 
 public class Main {
-
+//Flush Character program
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in) ;
-		System.out.println("Enter the word: ");
-			String s=sc.next();
-			
-				String str=UserMainCode.getMiddleChars(s);//function call
-				System.out.println("A Middle Charater:" +str);
-				sc.close();
+		try (// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the String: ");
+				String s1=sc.nextLine();
+				String s2= UserMainCode.getSpecialChar(s1);// function call
+				System.out.println(s2);
+		}
+		}
 
 	}
 
-}
+
--- UserMainCode.java
+++ UserMainCode.java
@@ -1,19 +1,16 @@
-package fetching_middle_character_from_string;
+package flushcharacters;
 
 public class UserMainCode {
 
-	public static String getMiddleChars(String s) {
+	public static String getSpecialChar(String s1) {
 		// TODO Auto-generated method stub
 		StringBuffer sb= new StringBuffer();
-		String s2=null;
-		if(s.length()%2==0) { // Constriant length
-		sb.append(s.substring(s.length()/2-1,s.length()/2+1));
-		// Stringlength=6;starting length=6/2-1,endinglength=6/2+1;(3-1,3+1);(2,4) 
-		}
-		 s2=sb.toString();// string coverted to  toString
-		return s2;
+		for(int i=0;i<s1.length();i++) {
+			char a =s1.charAt(i);
+			if(!Character.isAlphabetic(a))
+				sb.append(a);
 	}
-		
+		return sb.toString();
 	}
 
-
+}
