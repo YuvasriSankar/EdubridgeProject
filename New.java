ca8ab6aeb36deedfc91937ac5b7860012a0065ea
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-04-13 07:02:44 +0530

    Another Assessement program

--- New.java
+++ New.java
@@ -3,11 +3,6 @@ public class New {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int count=1;
-		while(count<=15) {
-			System.out.println(count%2==1?"***":"+++++");
-			++count;
-		}
 
 	}
 
