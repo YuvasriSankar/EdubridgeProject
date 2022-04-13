b902c679ffd6368cc6b752d8a04ecad6015f47bd
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-04-13 07:00:43 +0530

    Count program

---
+++ New.java
@@ -0,0 +1,14 @@
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=1;
+		while(count<=15) {
+			System.out.println(count%2==1?"***":"+++++");
+			++count;
+		}
+
+	}
+
+}
