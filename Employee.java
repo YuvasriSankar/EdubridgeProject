0ecd5ea6df73a397b70ee86efaa2927547f2cf02
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-04-14 11:50:53 +0530

    Employee Bonus program part-1

--- Employee.java
+++ Employee.java
@@ -1,75 +1,66 @@
-package polymorphism;
 
-public abstract class Employee {
-	private static String COMPANY_NAME="TCS";
-	private String empName;
-	private int empId;
-	private double salary;
 
-	public Employee(String empName, int empId, double salary) {
-		super();
-		this.empName = empName;
-		this.empId = empId;
-		this.salary = salary;
-	}
 
-	public Employee() {
-		super();
-	}
-	public void setCompanyName(String cname)
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Scanner;
+import java.util.Iterator;
+
+public class Employee {
+	public static TreeMap<Integer,Integer>
+	display(HashMap<Integer,String>hm,HashMap<Integer,Integer>hm1)
 	{
-		Employee.COMPANY_NAME=cname;
+	TreeMap<Integer,Integer> hm2=new TreeMap<Integer,Integer>();
+	Iterator<Integer> it=hm.keySet().iterator();
+	while(it.hasNext())
+	{
+	int y=it.next();
+	String dob=hm.get(y);
+	SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy");
+	sdf.setLenient(false);
+	String now="01-09-2014";
+	try
+	{ 
+		Date d=sdf.parse(now);
+	Date d1=sdf.parse(dob);
+	int y1=d.getYear();
+	int y2=d1.getYear();
+	int m1=d.getMonth();
+	int m2=d1.getMonth();
+	int day1=d.getDay();
+	int day2= d1.getDay();
+	int age=y1-y2;
+	if(m1<m2)
+	age--;
+	else if(m1==m2 && day1<day2)
+	age--;
+	if(age>=25 && age<=30)
+	{
+		float bonus=(float)0.2*hm1.get(y)+hm1.get(y);
+	hm2.put(y,(int)bonus );
 	}
-
-	public String getEmpName() {
-		return empName;
+	else if(age>30 && age<=60)
+	{ float bonus=(float) (0.3*hm1.get(y))+hm1.get(y);
+	
+	hm2.put(y,(int)bonus );
 	}
-
-	/*public void setEmpName(String empName) {
-		this.empName = empName;
-	}*/
-
-	public int getEmpId() {
-		return empId;
+	else if(age<25 || age>60)
+	hm2.put(y,-200);
+	else
+	hm2.put(y,-100);
 	}
-
-	/*public void setEmpId(int empId) {
-		this.empId = empId;
-	}*/
-
-	public double getSalary() {
-		return salary;
+	catch(Exception e)
+	{e.printStackTrace();
+	}}
+	return hm2;
 	}
-
-	/*@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + empId;
-		result = prime * result + ((empName == null) ? 0 : empName.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(salary);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}*/
-
-	@Override
-	public boolean equals(Object obj) {
-		Employee e1=(Employee)obj;
-		if(this.getEmpName().equals(e1.getEmpName()) && this.getEmpId()==e1.getEmpId() && this.getSalary()==e1.getSalary())
-		{
-			return true;
-		}else
-			return false;
-	}
-
-	protected String displayDetails() {
-		return empName;
-		
-		 
- 
-	}
-
-}
 	
 
+
+
+
+}
