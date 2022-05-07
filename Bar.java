f8929dcd52ff85ec175b55001060f6c63a073b94
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-07 22:51:20 +0530

    INNER AND OUTER PROGRAM

---
+++ Bar.java
@@ -0,0 +1,38 @@
+
+class Bar{
+		 class Bar1{
+			    public static void main (String [] args)   
+			  {      
+			  Foo f = new Foo();      
+			  f.makeBar(); 
+			   }   
+			    void makeBar()  
+			    {   
+			      (new Bar() {
+
+					public void go() {
+						// TODO Auto-generated method stub
+						
+					}}).go(); 
+			    }
+
+
+		 }
+}
+		//public void makeBar() {
+			// TODO Auto-generated method stub
+			
+	/* void makeBar()  
+	   {   
+	     (new Bar() {
+
+			public void go() {
+				// TODO Auto-generated method stub
+				
+			}}).go(); 
+	   }*/
+	
+
+
+
+
---
+++ Foo.java
@@ -0,0 +1,19 @@
+
+	public class Foo {   
+		 Foo()     {        System.out.print("foo");    } 
+
+		   class Bar{ 
+		   Bar()     {  
+		      System.out.print("bar");    
+		} 
+		   public void go()
+		     {    
+		    System.out.print("hi"); 
+		   }
+		}
+
+		public void makeBar() {
+			// TODO Auto-generated method stub
+			
+		}
+}
