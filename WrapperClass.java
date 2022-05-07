725f0322e58f7aa22c5e2f04b81e231a0e571121
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-07 22:39:11 +0530

    COMMAND LINE ARGUMENTS

---
+++ WrapperClass.java
@@ -0,0 +1,16 @@
+package wrapperclass;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String value1=new String("345");
+		int number=Integer.parseInt(value1);
+		System.out.println(number);
+
+		float fvalue=Float.parseFloat(value1);
+		System.out.println(fvalue);
+
+	}
+
+}
