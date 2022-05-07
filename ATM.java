40aa3c4677fa6f7dff00bcb71f19e6a5f62ee58f
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-07 22:19:51 +0530

    ATM MINI PROJECT IN ALL SOURCES CODE IN JAVA USING ELICSPE.

---
+++ ATM.java
@@ -0,0 +1,13 @@
+package atmproject;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+      OptionMenu optionMenu = new OptionMenu();
+      optionMenu.getLogin();
+	}
+
+}
---
+++ OptionMenu.java
@@ -0,0 +1,126 @@
+package atmproject;
+
+import java.io.IOException;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class OptionMenu extends Account {
+	Scanner menuInput = new Scanner(System.in);
+	DecimalFormat moneyFormat = new DecimalFormat("'$' ###,##0.00");
+	HashMap<Integer,Integer> data = new HashMap<Integer,Integer>();
+    public void getLogin() throws IOException{
+    	int x=1;
+    	do {
+    		try {
+    			data.put(948626,198765);
+    			data.put(948954,73396);
+    			
+    			System.out.println("************Welcome to the ATM Project***********");
+    			System.out.println("Enter your customer Number:");
+    			setCustomerNumber(menuInput.nextInt());
+    			
+    			System.out.println("Enter your PIN Number: ");
+    			setPinNumber(menuInput.nextInt());
+    			}catch(Exception e) {
+    			System.out.println("\n"+"Invalid Character(s).Only Numbers." +"\n");
+    			x=2;
+    		}
+    			int customernumber = getCustomerNumber();
+    			int pinnumber =getPinNumber();
+    			if(data.containsKey(customernumber)&& data.get(customernumber)==pinnumber) {
+    				getAccountType();
+    			}else
+    				System.out.println("\n"+"Wrong Customer Number or Pin Number"+"\n");
+    			
+    			} while(x==1);
+    	}
+    	public void getAccountType() {
+    		System.out.println("Select the Account you Want to Access: ");
+    		System.out.println("Type 1 -Checking Account");
+    		System.out.println("Type 2- Saving Account");
+    		System.out.println("Type 3 - Exit");
+    		int selection = menuInput.nextInt();
+    		switch(selection) {
+    		case 1:
+    			  getChecking();
+    			  break;
+    		case 2:
+  			  getSaving();
+  			  break;
+    		case 3:
+    			System.out.println("Thank you for using this ATM ,Have a nice day. \n");
+  			  break;
+    		default:
+    			System.out.println("\n"+"Invalid Choice"+"\n");
+    			getAccountType();
+    		}
+    	}
+    	public void getChecking() {
+    		System.out.println("Checking Account: ");
+    		System.out.println("Type 1 - View Balance");
+    		System.out.println("Type 2- Withdraw Funds");
+    		System.out.println("Type 3 - Deposit Funds");	
+    		System.out.println("Type 4- Exit");
+    		System.out.println("Choice: ");
+    		
+    		int selection = menuInput.nextInt();
+    		
+    		switch(selection) {
+    		    case 1:
+    			  System.out.println("Checking Account Balance:");
+    			  getAccountType();
+    			  break;
+    			case 2:
+    				  getCheckingWithdrawInput();
+        			  getAccountType();
+        			  break;
+    			case 3:
+  				  getCheckingDepositInput();
+      			  getAccountType();
+      			  break;
+    			case 4:
+    				System.out.println("Thank you for using this ATM ,Have a nice day. \n");
+    	  			  break;
+    	    	default:
+    	    			System.out.println("\n"+"Invalid Choice"+"\n");
+    	    			getChecking();
+    	    		}
+    	    	}
+    	public void getSaving() {
+    		System.out.println("Saving Account: ");
+    		System.out.println("Type 1 - View Balance");
+    		System.out.println("Type 2- Withdraw Funds");
+    		System.out.println("Type 3 - Deposit Funds");	
+    		System.out.println("Type 4 - Exit");
+    		System.out.println("Choice: ");
+    		int selection = menuInput.nextInt();
+    		
+    		switch(selection) {
+    		    case 1:
+    			  System.out.println("Saving Account Balance:" );
+    			  getAccountType();
+    			  break;
+    			case 2:
+    				  getSavingWithdrawInput();
+        			  getAccountType();
+        			  break;
+    			case 3:
+  				  getSavingDepositInput();
+      			  getAccountType();
+      			  break;
+    			case 4:
+    				System.out.println("Thank you for using this ATM ,Have a nice day. \n");
+    	  			  break;
+    	    	default:
+    	    			System.out.println("\n"+"Invalid Choice"+"\n");
+    	    			getSaving();
+    	    		}
+    	    	}
+    		
+    	}
+	
+
+
+
