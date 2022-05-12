cb0ddb4f6ef7928a2c820f123fb627bdddd0f3eb
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-12 20:12:15 +0530

    HibernateMavenProject

--- Employee.java
+++ Employee.java
@@ -1,66 +1,56 @@
+package com.edubridge.hibernate.model;
 
+import javax.persistence.Column;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.TreeMap;
-import java.util.Scanner;
-import java.util.Iterator;
-
+@Entity
+@Table(name= "emp300")
 public class Employee {
-	public static TreeMap<Integer,Integer>
-	display(HashMap<Integer,String>hm,HashMap<Integer,Integer>hm1)
-	{
-	TreeMap<Integer,Integer> hm2=new TreeMap<Integer,Integer>();
-	Iterator<Integer> it=hm.keySet().iterator();
-	while(it.hasNext())
-	{
-	int y=it.next();
-	String dob=hm.get(y);
-	SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy");
-	sdf.setLenient(false);
-	String now="01-09-2014";
-	try
-	{ 
-		Date d=sdf.parse(now);
-	Date d1=sdf.parse(dob);
-	int y1=d.getYear();
-	int y2=d1.getYear();
-	int m1=d.getMonth();
-	int m2=d1.getMonth();
-	int day1=d.getDay();
-	int day2= d1.getDay();
-	int age=y1-y2;
-	if(m1<m2)
-	age--;
-	else if(m1==m2 && day1<day2)
-	age--;
-	if(age>=25 && age<=30)
-	{
-		float bonus=(float)0.2*hm1.get(y)+hm1.get(y);
-	hm2.put(y,(int)bonus );
-	}
-	else if(age>30 && age<=60)
-	{ float bonus=(float) (0.3*hm1.get(y))+hm1.get(y);
-	
-	hm2.put(y,(int)bonus );
-	}
-	else if(age<25 || age>60)
-	hm2.put(y,-200);
-	else
-	hm2.put(y,-100);
-	}
-	catch(Exception e)
-	{e.printStackTrace();
-	}}
-	return hm2;
-	}
-	
 
+@Id
+@GeneratedValue
+@Column(name="emp_id")
+private int id;
+@Column(name="first_Name",length=20,unique=true)
+private String firstName;
+@Column(name="last_Name",nullable=false)
+private String lastName;
+@Transient
+private double salary;
 
+public double getSalary() {
+	return salary;
+}
+public void setSalary(double salary) {
+	this.salary = salary;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
 
 
 }
+
+
+
+
--- Test.java
+++ Test.java
@@ -1,25 +1,53 @@
-package test;
+package com.edubridge.hibernate.model;
 
+
+
+import java.util.Scanner;
+
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+  
 public class Test {
-	
-	int testId;//primary key
-	String testName;
-	
-public Test()
-{
-	       
-}
-public int getTestId() {
-	return testId;
-}
-public void setTestId(int testId) {
-	this.testId = testId;
-}
-public String getTestName() {
-	return testName;
-}
-public void setTestName(String testName) {
-	this.testName = testName;
-}
+public static void main(String[] args) {
+	try{
+		Configuration cfg = new Configuration();
+	    System.out.println("2222222222222222222222222222");
+	    cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file  
+	    SessionFactory factory=cfg.buildSessionFactory();  
+	    System.out.println("4444444444444444444444444");
+		
+		Session session = factory.openSession();
+		Transaction t = null;
 
+	/*Session session=new AnnotationConfiguration().configure().buildSessionFactory().openSession();
+	
+	Transaction t=session.beginTransaction();*/
+	Scanner sn=new Scanner(System.in);
+	for(int i=0;i<5;i++)
+	{
+		t=session.beginTransaction();
+	
+	Employee e1=new Employee();
+	//e1.setId(1234);// autoincreament in setid.
+	e1.setFirstName(sn.next());
+	e1.setLastName(sn.next());
+	
+	/*Employee e2=new Employee();
+	//e2.setId(5678);
+	e2.setFirstName("mohan");
+	e2.setLastName("patil");*/
+	
+	session.persist(e1);
+	//session.persist(e2);
+	
+	t.commit();
+	}
+	session.close();
+	System.out.println("successfully saved");
+	}catch(Exception ex)
+	{
+		System.out.println("exception "+ex.getMessage());
+	}
+}
 }
