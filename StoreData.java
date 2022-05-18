15b223587f56522eeb326407c452c623604a652d
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-18 11:41:07 +0530

    HIBERNATE ONE TO MANY EXAMPLE MAPPING BY USING ANNOTATION

--- StoreData.java
+++ StoreData.java
@@ -1,55 +1,95 @@
-package ex;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
-public class StoreData {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Storing the data");
-		try {
-			// creating configuration object
-			System.out.println("111111111111111111111");
-			Configuration cfg = new Configuration();
-			System.out.println("2222222222222222222222222222");
-			cfg.configure("Hibernate.cfg.xml");// populates the data of the
-												// configuration file
-			System.out.println("3333333333333333333333333333");
-			// creating seession factory object
-			SessionFactory factory = cfg.buildSessionFactory();
-			System.out.println("4444444444444444444444444");
-			// creating session object
-			Session session = factory.openSession();
-			System.out.println("5555555555555555555555555");
-			// creating transaction object
-			Transaction t = session.beginTransaction();
-			System.out.println("66666666666666666666666666666666");
-			OrderDetails e1 = new OrderDetails();
-			e1.setOrderID(200);
-			ContactDetails holidayContact = new ContactDetails();
-			holidayContact.setAddress("Chennai");
-			holidayContact.setPhone("7339643931");
-			holidayContact.setRecipient("YuvasriSankar");
-			e1.setHolidayContact(holidayContact);
-			ContactDetails weekdayContact = new ContactDetails();
-			weekdayContact.setAddress("Vandavasi");
-			weekdayContact.setPhone("9486264607");
-			weekdayContact.setRecipient("Gomathi");
-			e1.setWeekdayContact(weekdayContact);
-			System.out.println("7777777777777777777777777777777");
-			session.save(e1);// persisting the object
-			System.out.println("888888888888888888888888888888");
-			t.commit();// transaction is committed
-			session.close();
-
-			System.out.println("successfully saved");
-		} catch (Exception ex) {
-			System.out.println("Problem in connection" + ex.getMessage());
-			System.out.println(ex.getCause());
-		}
-	}
-
-}
+	package com.mypack;    
+	  
+	import java.util.ArrayList;    
+	  
+	import org.hibernate.Session;  
+	import org.hibernate.SessionFactory;  
+	import org.hibernate.Transaction;  
+	//import org.hibernate.boot.Metadata;  
+	//import org.hibernate.boot.MetadataSources;  
+	//import org.hibernate.boot.registry.StandardServiceRegistry;  
+	//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+    import org.hibernate.cfg.Configuration;  
+	public class StoreData {    
+	public static void main(String[] args) {    
+	     
+	   /* StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+	    Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();  
+	      
+	    SessionFactory factory=meta.getSessionFactoryBuilder().build(); */ 
+		 Configuration cfg=new Configuration();
+		    System.out.println("2222222222222222222222222222");
+		    cfg.configure("Hibernate.cfg.xml");//populates the data of the configuration file  
+		    System.out.println("3333333333333333333333333333");
+		    //creating session factory object  
+		    SessionFactory factory=cfg.buildSessionFactory();  
+		    System.out.println("4444444444444444444444444");
+		    //creating session object  
+	    Session session=factory.openSession();  
+	      
+	    Transaction t=session.beginTransaction();    
+	        
+	    Answer ans1=new Answer();    
+	    ans1.setAnswername("Java is a programming language");    
+	    ans1.setPostedBy("yuvasri");    
+	        
+	    Answer ans2=new Answer();    
+        ans2.setAnswername("Java is a platform");    
+	    ans2.setPostedBy("Gomathi");    
+	        
+	    Answer ans3=new Answer();    
+	    ans3.setAnswername("Servlet is an Interface");    
+	    ans3.setPostedBy("Keerthana");    
+	        
+	    Answer ans4=new Answer();    
+	    ans4.setAnswername("Servlet is an API");    
+	    ans4.setPostedBy("Keerthikshah");  
+	    
+	    Answer ans5=new Answer();    
+	    ans5.setAnswername("Mapping tool");    
+	    ans5.setPostedBy("Thilagavathi");
+	    
+	    Answer ans6=new Answer();    
+	    ans6.setAnswername("Tool for doing database transaction");    
+	    ans6.setPostedBy("sankar");
+	        
+	    ArrayList<Answer> list1=new ArrayList<Answer>();    
+	    list1.add(ans1);    
+	    list1.add(ans2);    
+	        
+	    ArrayList<Answer> list2=new ArrayList<Answer>();    
+	    list2.add(ans3);    
+	    list2.add(ans4);  
+	    
+	    ArrayList<Answer> list3=new ArrayList<Answer>();    
+	    list3.add(ans5); 
+	    
+	    ArrayList<Answer> list4=new ArrayList<Answer>(); 
+	    list4.add(ans6);  
+	        
+	    Question question1=new Question();    
+	    question1.setQname("What is Java?");    
+	    question1.setAnswers(list1);    
+	        
+	    Question question2=new Question();    
+	    question2.setQname("What is Servlet?");    
+	    question2.setAnswers(list2); 
+	    
+	    Question question3=new Question();    
+	    question3.setQname("What is hibernate?");    
+	    question3.setAnswers(list3);  
+	    
+	    Question question4=new Question();    
+	    question4.setQname("What is difference between final and finally?");    
+	    question4.setAnswers(list4);   
+	    
+	    session.persist(question1);    
+	    session.persist(question2);
+	    session.persist(question3);    
+	    session.persist(question4);
+	        
+	    t.commit();    
+	    session.close();    
+	    System.out.println("successfully saved");    
+	}    
+	}   
