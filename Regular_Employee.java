685af68f0edaa857a3cb62d140b0e536bd9035ec
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-22 19:55:10 +0530

    HIBERNATE INHERITANCE TABLE PER SUBCLASS PROGRAM

--- Regular_Employee.java
+++ Regular_Employee.java
@@ -1,34 +1,18 @@
-package com.tableperhierarchyusingannotation;
+package com.tablepersubclass;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-	//import javax.persistence.*;  
-@Entity  
-		@DiscriminatorValue("regularemployee")  
-	public class Regular_Employee extends Employee{    
-		      
-		@Column(name="salary")    
-		private float salary;  
-		  
-		@Column(name="bonus")     
-		private int bonus;
-
-		public float getSalary() {
-			return salary;
-		}
-
-		public void setSalary(float salary) {
-			this.salary = salary;
-		}
-
-		public int getBonus() {
-			return bonus;
-		}
-
-		public void setBonus(int bonus) {
-			this.bonus = bonus;
-		}  
-
-
+	public class Regular_Employee extends Employee{  
+	private float salary;  
+	private int bonus;
+public float getSalary() {
+	return salary;
 }
+public void setSalary(float salary) {
+	this.salary = salary;
+}
+public int getBonus() {
+	return bonus;
+}
+public void setBonus(int bonus) {
+	this.bonus = bonus;
+}  
+	}
--- StoreData.java
+++ StoreData.java
@@ -1,47 +1,41 @@
-package com.tableperhierarchyusingannotation;
+package com.tablepersubclass;
 
-		import org.hibernate.Session;  
-	    import org.hibernate.SessionFactory;  
-		import org.hibernate.Transaction;
-        import org.hibernate.cfg.Configuration;  
-		  
-		public class StoreData {  
-		  
-		    public static void main(String args[]) {  
-	      
-		    	 Configuration cfg=new Configuration();
-		 	    System.out.println("2222222222222222222222222222");
-		 	    cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file  
-		 	    System.out.println("3333333333333333333333333333");
-		 	    //creating session factory object  
-		 	    SessionFactory factory=cfg.buildSessionFactory();
-    
-		        Session session=factory.openSession();  
-		          
-		         Transaction t=session.beginTransaction();    
-	            
-		            Employee e1=new Employee();    
-		            e1.setName("Gaurav Chawla");    
-		                
-		            Regular_Employee e2=new Regular_Employee();    
-		            e2.setName("Vivek Kumar");    
-	            e2.setSalary(50000);    
-		            e2.setBonus(5);    
-		                
-		            Contract_Employee e3=new Contract_Employee();    
-		            e3.setName("Arjun Kumar");    
-		            e3.setPay_per_hour(1000);    
-		            e3.setContract_duration("15 hours");    
-	                
-		            session.persist(e1);    
-		            session.persist(e2);    
-		            session.persist(e3);    
-	                
-	            t.commit();    
-		            session.close();    
-		            System.out.println("success");        
-		    }  
-		 
+	import org.hibernate.Session;  
+	import org.hibernate.SessionFactory;  
+	import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;  
+	public class StoreData {  
+	public static void main(String[] args) {  
 
-
+   	 Configuration cfg=new Configuration();
+	    System.out.println("2222222222222222222222222222");
+	    cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file  
+	    System.out.println("3333333333333333333333333333");
+	    //creating session factory object  
+	    SessionFactory factory=cfg.buildSessionFactory();
+  
+	Session session=factory.openSession();  
+	  
+	Transaction t=session.beginTransaction();    
+	  
+	Employee e1=new Employee();    
+	e1.setName("Gaurav Chawla");    
+	Regular_Employee e2=new Regular_Employee();    
+	e2.setName("Vivek Kumar");    
+	e2.setSalary(50000);    
+	e2.setBonus(5);    
+	    
+	Contract_Employee e3=new Contract_Employee();    
+	e3.setName("Arjun Kumar");    
+	e3.setPay_per_hour(1000);    
+	e3.setContract_duration("15 hours");    
+	    
+	session.persist(e1);    
+	session.persist(e2);    
+	session.persist(e3);    
+	    
+	t.commit();    
+	session.close();    
+	System.out.println("success");    
+}
 }
