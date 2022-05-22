cac69dd9a6986ca77a7fe7945e6a0f0b7f908120
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-22 19:59:48 +0530

    HIBERNATE INHERITANCE TABLE PER SUBCLASS USING ANNOTATION

--- Employee.java
+++ Employee.java
@@ -1,21 +1,34 @@
-package com.tablepersubclass;
+package com.tablepersubclassusingannotation;
 
-public class Employee {
-			private int id;  
-			private String name;
-			public int getId() {
-				return id;
-			}
-			public void setId(int id) {
-				this.id = id;
-			}
-			public String getName() {
-				return name;
-			}
-			public void setName(String name) {
-				this.name = name;
-			}  
-			  
+	import javax.persistence.*;  
+	  
+	@Entity  
+	@Table(name = "employee103")  
+	@Inheritance(strategy=InheritanceType.JOINED)  
+	  
+	public class Employee {  
+	@Id  
+	@GeneratedValue(strategy=GenerationType.AUTO)  
+	      
+	@Column(name = "id")  
+	private int id;  
+	  
+	@Column(name = "name")  
+	private String name;
 
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}  
 }
