4e57806446a813120321720d802261cc027e9098
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-22 19:46:24 +0530

    HIBERNATE INHERITANCE TABLE PER HIERARCHY PROGRAM

--- Employee.java
+++ Employee.java
@@ -1,34 +1,20 @@
-package com.tableperconcreteclassusingannotation;
-	import javax.persistence.*;  
-		@Entity  
-		@Table(name = "employee102")  
-		@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)  
-		  
-		public class Employee {  
-		@Id  
-		@GeneratedValue(strategy=GenerationType.AUTO)  
-		      
-		@Column(name = "id")  
-		private int id;  
-		  
-		@Column(name = "name")  
-		private String name;
+package com.tableperhierarchy;
 
+public class Employee {
+		private int id;  
+		private String name;
 		public int getId() {
 			return id;
 		}
-
 		public void setId(int id) {
 			this.id = id;
 		}
-
 		public String getName() {
 			return name;
 		}
-
 		public void setName(String name) {
 			this.name = name;
 		}  
 
-		}
 
+}
--- Regular_Employee.java
+++ Regular_Employee.java
@@ -1,31 +1,17 @@
-package com.tableperconcreteclassusingannotation;
-	import javax.persistence.*;  
-		@Entity  
-		@Table(name="regularemployee102")  
-		@AttributeOverrides({  
-	    @AttributeOverride(name="id", column=@Column(name="id")),  
-		@AttributeOverride(name="name", column=@Column(name="name"))  
-		})  
-		public class Regular_Employee extends Employee{  
-		      
-		@Column(name="salary")    
-		private float salary;  
-		  
-		@Column(name="bonus")     
-		private int bonus;
+package com.tableperhierarchy;
 
+public class Regular_Employee extends Employee {
+		private float salary;  
+		private int bonus;
 		public float getSalary() {
 			return salary;
 		}
-
 		public void setSalary(float salary) {
 			this.salary = salary;
 		}
-
 		public int getBonus() {
 			return bonus;
 		}
-
 		public void setBonus(int bonus) {
 			this.bonus = bonus;
 		}  
