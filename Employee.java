3f22e46a458984edb3b9ea920f8428e79b327bac
YuvasriSankar <101916779+YuvasriSankar@users.noreply.github.com>
2022-05-24 21:39:29 +0530

    CRITERIA QUERIES PROGRAM USING HIBERNATE CACHE

--- Employee.java
+++ Employee.java
@@ -1,34 +1,49 @@
-package com.tablepersubclassusingannotation;
+package criteriaqueries;
 
-	import javax.persistence.*;  
-	  
-	@Entity  
-	@Table(name = "employee103")  
-	@Inheritance(strategy=InheritanceType.JOINED)  
-	  
-	public class Employee {  
-	@Id  
-	@GeneratedValue(strategy=GenerationType.AUTO)  
-	      
-	@Column(name = "id")  
-	private int id;  
-	  
-	@Column(name = "name")  
-	private String name;
+	public class Employee {
+		   private int id;
+		   private String firstName; 
+		   private String lastName;   
+		   private int salary;  
 
-	public int getId() {
-		return id;
-	}
+		   public Employee() {}
+		   
+		   public Employee(String fname, String lname, int salary) {
+		      this.firstName = fname;
+		      this.lastName = lname;
+		      this.salary = salary;
+		   }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+		public int getId() {
+			return id;
+		}
 
-	public String getName() {
-		return name;
-	}
+		public void setId(int id) {
+			this.id = id;
+		}
 
-	public void setName(String name) {
-		this.name = name;
-	}  
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public int getSalary() {
+			return salary;
+		}
+
+		public void setSalary(int salary) {
+			this.salary = salary;
+		}
+		   
 }
